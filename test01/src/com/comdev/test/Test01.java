73f59fe7e6d6abe8fe43d6d77019713112d64477
zhushaolong <zhushaolong@gmail.com>
2013-04-18 07:51:45 +0800

    msg

---
+++ test01/src/com/comdev/test/Test01.java
@@ -0,0 +1,22 @@
+package com.comdev.test;
+
+/**
+ * Author: gnoloahs
+ * Date: 2013-04-17
+ * Time: 上午8:44
+ */
+public class Test01
+{
+
+    public static void main(String[] args) throws Exception
+    {
+
+
+          com.sun.tools.javac.Main.main(new String[]{"D:\\work_tests\\test01\\src\\com\\comdev\\test\\Run.java"});
+
+
+
+    }
+
+
+}
---
+++ test01/src/com/comdev/test/getJs.java
@@ -0,0 +1,20 @@
+package com.comdev.test;
+
+/**
+ * Author: gnoloahs
+ * Date: 2013-04-17
+ * Time: 上午11:54
+ */
+public class getJs
+{
+
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+
+
+}
