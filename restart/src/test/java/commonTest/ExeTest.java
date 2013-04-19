61cd9f82d6151d5d3c69b3aa727ac2c43b3252c7
zhushaolong <zhushaolong@gmail.com>
2013-04-19 19:46:31 +0800

    msg

---
+++ restart/src/test/java/commonTest/ExeTest.java
@@ -0,0 +1,26 @@
+package commonTest;
+
+import com.me.ut.exe.RunExe;
+import com.me.ut.string.StringUT;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Author: gnoloahs
+ * Date: 2013-04-19
+ * Time: 上午11:30
+ */
+public class ExeTest
+{
+
+    public static final Logger logger = Logger.getLogger(ExeTest.class);
+
+    @Test
+    public void test01()
+    {
+
+
+    }
+
+
+}
